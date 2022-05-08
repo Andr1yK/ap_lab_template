939f48e73d985c628d2598d999401795710114e6
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-05-08 12:19:54 +0300

    Remove build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andriyk/Documents/DSPU/AP/ap_lab_template
-# Build directory: /Users/andriyk/Documents/DSPU/AP/ap_lab_template/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
